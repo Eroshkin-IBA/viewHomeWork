2fced1a8c1481447ccc0d4b251ad3279fdf1e377
Eroshkin-IBA <eroshkin.artyom.1.21@gmail.com>
2023-09-06 12:57:54 +0300

    task is finished

--- app/src/main/java/com/example/viewhomework/MainActivity.kt
+++ app/src/main/java/com/example/viewhomework/MainActivity.kt
@@ -1,11 +1,55 @@
 package com.example.viewhomework
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var editTextImageUrl: EditText
+    private lateinit var imageView: ImageView
+    private lateinit var buttonLoadImage: Button
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        editTextImageUrl = findViewById(R.id.editTextImageUrl)
+        imageView = findViewById(R.id.imageView)
+        buttonLoadImage = findViewById(R.id.buttonLoadImage)
+
+        buttonLoadImage.setOnClickListener {
+            val imageUrl = editTextImageUrl.text.toString()
+            if (imageUrl.isNotEmpty()) {
+                loadImage(imageUrl)
+            } else {
+                Toast.makeText(this@MainActivity, "Введите URL изображения", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
+    private fun loadImage(imageUrl: String) {
+        Picasso.get()
+            .load(imageUrl)
+            .into(imageView, object : Callback {
+                override fun onSuccess() {
+                    imageView.visibility = View.VISIBLE
+                }
+
+                override fun onError(e: Exception?) {
+                    Toast.makeText(this@MainActivity, "Ошибка при загрузке изображения", Toast.LENGTH_SHORT).show()
+                }
+            })
     }
 }
